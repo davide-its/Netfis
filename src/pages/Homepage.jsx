8e3b5c75576581dbd47e88e4d7bf5181941ae0c3
davide-its <davide.martinelli@edu-its.it>
2025-11-07 12:32:46 +0100

    init: inizializzazione del progetto con stesura di rotte e aggiunta di tailwindcss

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+
+export default function Homepage() {
+    return (
+        <>
+            <div className="container">
+                <h1 className="m-auto border-2">Scacated</h1>
+            </div>
+        </>
+    );
+}
