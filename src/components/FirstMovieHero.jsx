a85e5b70b7e553bd115772c37cdeef2503809a76
davide-its <davide.martinelli@edu-its.it>
2025-11-17 12:23:02 +0100

    Fix per responsive video

--- src/components/FirstMovieHero.jsx
+++ src/components/FirstMovieHero.jsx
@@ -3,27 +3,26 @@ import Button from "./Button";
 
 export default function FirstMovieHero({ firstMovie, firstMovieImage, firstMovieTrailer }) {
     return (
-        <div className="relative flex items-center h-[70vh] w-full overflow-hidden mx-auto px-10 lg:px-56">
+        <div className="relative flex items-center h-fit lg:h-[70vh]  flex-col lg:flex-row 2xl:h-screen w-full overflow-hidden mx-auto px-10 lg:px-56 ">
             {/* Background image */}
-            {/* <img
+            <img
                 src={`https://image.tmdb.org/t/p/original${firstMovieImage}`}
                 alt={firstMovie?.title}
-                className="absolute inset-0 w-full h-full object-cover object-top"
-            /> */}
+                className="absolute inset-0 w-full h-full object-cover object-top opacity-30"
+            />
+
+            <div className="absolute inset-0 top-0 left-0 w-4/5 bg-linear-to-r from-black to-transparent z-10 opacity-0 lg:opacity-100"></div>
+            <div className="absolute inset-y-0 top-0 right-0 w-1/2 bg-linear-to-l from-black to-transparent z-10 opacity-0 lg:opacity-100"></div>
+
 
             <iframe
                 src={`https://www.youtube.com/embed/${firstMovieTrailer}?autoplay=1&mute=1&controls=0&loop=1&playlist=${firstMovieTrailer}`}
-                className="absolute inset-0 w-full h-full object-cover object-top"
-                allow="autoplay; encrypted-media; picture-in-picture"
+                className="lg:absolute top-0 left-0 w-screen h-full object-cover object-center opacity-0 lg:opacity-100"
+                allow="autoplay; encrypted-media;"
                 allowFullScreen
                 style={{ pointerEvents: "none" }}
             />
-            {/* Gradient laterali */}
-            <div className="absolute inset-y-0 left-125 w-1/4 bg-gradient-to-r from-black to-transparent z-10 "></div>
-            <div className="absolute inset-y-0 right-125 w-1/10 bg-gradient-to-l from-black to-transparent z-10"></div>
 
-            {/* Gradient in basso */}
-            <div className="absolute inset-x-0 bottom-0 h-1/5 bg-gradient-to-t from-black to-transparent z-20"></div>
             {/* Content */}
 
             <div className="relative z-20 max-w-2xl">
